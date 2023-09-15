d5494d05fe89a18030cfb999f4ed994a33c43dbb
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-09-15 13:55:12 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 
 //routes
-    //Greeting
-    app.get('/greeting', (req, res) => {
-        res.send('<h1>Hello stranger</h1>');
-    });
 
-    // //Greeting w/ param
-    app.get('/greeting/:name', (req, res) => {
-        res.send(`<h1>What's up ${req.params.name}</h1>`);
-    });
+    //GREETINGS
+        //Greeting
+            // app.get('/greeting', (req, res) => {
+            //     res.send('<h1>Hello stranger</h1>');
+            // });
+
+        // //Greeting w/ param
+            // app.get('/greeting/:name', (req, res) => {
+            //     res.send(`<h1>What's up ${req.params.name}</h1>`);
+            // });
+
+    //TIP CALCULATOR
+        app.get('/tip/:total/:tipPercentage', (req, res) => {
+            res.send(`<h1> Your tip is $${req.params.total * (req.params.tipPercentage / 100)}</h1>`);
+        });
 
 app.listen(3000, () => {
     console.log('listening');
