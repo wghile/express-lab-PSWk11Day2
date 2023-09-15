0b29ac2cb3ed3813e5b86215fce254debdc4b904
Wentana Ghile <wentanaghile@MacBook-Pro.local>
2023-09-15 14:09:21 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -15,9 +15,22 @@ const app = express();
             // });
 
     //TIP CALCULATOR
-        app.get('/tip/:total/:tipPercentage', (req, res) => {
-            res.send(`<h1> Your tip is $${req.params.total * (req.params.tipPercentage / 100)}</h1>`);
-        });
+        // app.get('/tip/:total/:tipPercentage', (req, res) => {
+        //     res.send(`<h1> Your tip is $${req.params.total * (req.params.tipPercentage / 100)}</h1>`);
+        // });
+
+    //MAGIC 8 BALL
+
+        const randomResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+        let arrayLength = randomResponse.length - 1
+
+        app.get('/magic/:question', (req, res) => {
+            res.send(
+                `<h1>Question: ${req.params.question}</h1>
+                <h1>Magic 8 Ball: ${randomResponse[Math.floor(Math.random() * (arrayLength + 1))]}</h1>`
+            )
+        })
 
 app.listen(3000, () => {
     console.log('listening');
